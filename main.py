e501fa98617c9266cbe64f9cfad5daa52180efbf
nithishbn <nithishthegreat7@gmail.com>
2017-01-20 12:26:13 -0800

    Made admin class

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import _sqlite3 as sql
 
-class Run():
+class Admin():
     def __init__(self):
-        sqft = input("Que es su sqft count because we're creepy")
-        bed = input("And sus camas?")
-        bath = input("BANOS")
-        desired_price = input("What do you need in money?")
-        self.calculate(sqft, bed, bath, desired_price)
-    def calculate(self, sqft, bed, bath, desired_price):
+        sqft = input("Square foot: \n")
+        bed = input("Bed: \n")
+        bath = input("Bath: \n")
+        cost = input("Cost: \n")
+        self.enter_data(sqft, bed, bath, cost)
+    def enter_data(self, sqft, bed, bath, cost):
         self.sqft = sqft
         self.bed = float(bed)
         self.bath = float(bath)
-        self.desired_price = desired_price
+        self.cost = cost
         conn = sql.connect("data.sqlite")
         cur = conn.cursor()
         cur.execute('''Create Table if not exists main (cost integer, sqft integer, bed integer, bath integer)''')
-        cur.execute('''Create Table if not exists user (desired integer, sqft integer, bed integer, bath integer''')
-        cur.execute('''Insert into (cost, sqft, bed, bath) VALUES (?, ?, ?, ?)''', ())
-        return "Dang your house is sick dude!\nGlad to take it from you!"
+        cur.execute(''''Insert into main (cost, sqft, bed, bath) values (?, ?, ?, ?)''', (cost, sqft, bed, bath))
+        conn.commit()
+admin = Admin()
 
-r = Run()
-print(r.calculate(10000, 0, 0, 1000000))
