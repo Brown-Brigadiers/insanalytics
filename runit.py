55a0c5a586d062efb9d4b08c30818547e73118b8
Nithish <nithishthegreat7@gmail.com>
2017-01-25 19:23:05 -0800

    Deleted calculate.py
    made it part of user class

---
+++ runit.py
@@ -0,0 +1,13 @@
+import _sqlite3 as sql
+import numpy
+from interface import Interface
+from admin import Admin
+from user import User
+
+# c = Calculate()
+# admin = Admin()
+i = Interface()
+user = User(538000, 1000, 4, 3)
+# user.databasesearch()
+user.linear_regression()
+# TODO make this into a UI or callable from another program
