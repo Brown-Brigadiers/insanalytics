fbe7c469a5d2dc13177661ef9e3c62ff0c23fa6a
Nithish <nithishthegreat7@gmail.com>
2017-01-25 21:55:02 -0800

    Button! :D

--- interface.py
+++ interface.py
@@ -1,4 +1,16 @@
-import kivy
-class Interface(object):
-    def __init__(self):
-        pass
+from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button
+
+
+class Interface(App):
+    def build(self):
+        a = AnchorLayout(anchor_x='center', anchor_y='top', padding=[10,100,100,100])
+        bu=Button(text="Nithish")
+        bu.size_hint = (0.5, 0.2)
+        a.add_widget(bu)
+        return a
+
+
+Interface().run()
