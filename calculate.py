f181e04cca313f7d87475b9bf38e89f252bc20ea
Nithish <nithishthegreat7@gmail.com>
2017-01-25 17:52:38 -0800

    ASD

--- calculate.py
+++ calculate.py
@@ -1,2 +1,20 @@
+from user import User
+# calculate class derived from User
+class Calculate(User):
+    def __init__(self, dprice, sqft, bed, bath):
+        User.__init__(self, dprice, sqft, bed, bath)
+        # initializes with User's values
+
+    def linear_regression(self):
+        dprice = self.dprice
+        sqft = self.sqft
+        bed = self.bed
+        bath = self.bath
+        conn = sql.connect("data.sqlite")
+        cur = conn.cursor()
+        # selects ALLLL the x data that is not cost
+        cur.execute('''SELECT sqft, bed, bath from Main''')
+        allthedata = cur.fetchall()
+        print(allthedata)
 
 
