ef0ad437f990fb0a44d76accdeda8bf700a3c63d
nithishbn <nithishthegreat7@gmail.com>
2017-01-27 09:24:27 -0800

    KV Language File added

--- interface.py
+++ interface.py
@@ -1,16 +1,24 @@
 from kivy.app import App
-# from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 
 class Interface(App):
     def build(self):
-        a = AnchorLayout(anchor_x='center', anchor_y='top', padding=[10,100,100,100])
-        bu=Button(text="Nithish")
-        bu.size_hint = (0.5, 0.2)
-        a.add_widget(bu)
-        return a
-
-
+        Builder.load_file('Interface.kv')
+        g = GridLayout()
+        l = Label()
+        t = TextInput()
+        l1 = Label()
+        t1 = TextInput()
+        g.add_widget(l)
+        g.add_widget(t)
+        g.add_widget(l1)
+        g.add_widget(t1)
+        return g
 Interface().run()
