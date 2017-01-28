7b5017b12d2cbdc2533f4f3f74f3354ee19fb060
Nithish <nithishthegreat7@gmail.com>
2017-01-27 16:48:45 -0800

    random kivy stuff

--- interface.py
+++ interface.py
@@ -1,15 +1,26 @@
+
 from kivy.app import App
-# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.utils import escape_markup
 
 
 class Interface(App):
     def build(self):
-        a = AnchorLayout(anchor_x='center', anchor_y='top', padding=[10,100,100,100])
-        bu=Button(text="Nithish")
-        bu.size_hint = (0.5, 0.2)
-        a.add_widget(bu)
+        Window.clearcolor = (1, 1, 1, 1)
+        a = BoxLayout(orientation='horizontal')
+        a = BoxLayout(spacing=10)
+        text="Username"
+        l = Label(text='[color #000000]' + escape_markup(text) + '[/b]',  markup = True)
+        t=TextInput(focus=True)
+        l.size_hint = (0.1, 0.2)
+        t.size_hint=(0.1,0.2)
+        a.add_widget(l)
+        a.add_widget(t)
         return a
 
 
