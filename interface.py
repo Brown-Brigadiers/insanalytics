ab5868fff57734ca7139c01ab5e85262e7c5ac5b
Nithish <nithishthegreat7@gmail.com>
2017-01-29 20:43:58 -0800

    Got basic layout of interface

--- interface.py
+++ interface.py
@@ -1,18 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.properties import ListProperty
 
+class UserInput(BoxLayout):
+    values = ListProperty()
+    def __init__(self, **kwargs):
+        super(UserInput, self).__init__(**kwargs)
+    # def on_press(self):
+    #         self.values =
 
 class Interface(App):
     def build(self):
-        a = AnchorLayout(anchor_x='center', anchor_y='top', padding=[10,100,100,100])
-        bu=Button(text="Nithish")
-        bu.size_hint = (0.5, 0.2)
-        a.add_widget(bu)
-        return a
-
+        return UserInput()
+Interface().run()
